08d93b7f3766308dba9475c4f8e8e185de427690
Brawlup <86495381+brawlup@users.noreply.github.com>
2021-07-03 14:06:56 +0300

    Create club.js

---
+++ src/club.js
@@ -0,0 +1,56 @@
+class Club {
+  constructor(data) {
+    this.tag = data.tag
+    this.name = data.name
+    this.type = data.type
+    this.badge = data.badgeId
+    this.description = data.description
+    this.trophies = data.trophies
+    this.requiredTrophies = data.requiredTrophies
+    this.members = data.members
+    this.memberCount = data.members.length
+    this.isFull = this.memberCount === 100 ? true : false
+  }
+
+  /**
+  * @param {string} Player tag
+  * @returns {number} Player's club rank | null (if the player is not in this club)
+  */
+
+  getMemberRank(tag) {
+    let sort = this.members.sort((a, b) => b.trophies - a.trophies).map(x => x.tag).indexOf(tag)+1
+    return sort >= 1 ? parseInt(sort) : null
+  }
+
+  /**
+  * @param {string} Player tag
+  * @returns {string} Player's club rank (member,if the player is not in this club)
+  */
+
+  getMemberRole(tag) {
+    return !this.members.filter(m => m.tag == tag) ? TypeError("This member isn't in a club") : this.members.filter(m => m.tag == tag).map(m => m.role).join("\n")
+  }
+
+  /**
+  * @param {string} tag Player TAG
+  * @description check if the player can join the club
+  * @returns {boolean}
+  */
+
+  playerCanJoin(tag) {
+    let x = this.members.map(x => x.tag)
+    if (x.includes(tag)) return true
+    return this.members.filter(m => m.tag == tag).map(x => x.trophies).join("") >= this.requiredTrophies
+  }
+
+  /**
+  * @description sort club members by trophies
+  * @returns {object} object
+  */
+
+  sortMembersByTrophies() {
+    return !this.members ? TypeError("Invalid Club") : this.members.sort((a, b) => b.trophies - a.trophies)
+  }
+}
+
+module.exports = Club
