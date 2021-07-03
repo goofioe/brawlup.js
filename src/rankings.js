00c61efa6a485a430e1bb805d5b092a9c87c4215
Brawlup <86495381+brawlup@users.noreply.github.com>
2021-07-03 15:51:48 +0300

    Create rankings.js

---
+++ src/rankings.js
@@ -0,0 +1,33 @@
+class Ranking {
+    constructor(data) {
+        this.country = data.country
+        this.type = data.type   
+        this.ranks = data.ranks["items"]
+        this.rankCount = data.ranks["items"].length
+    }
+
+    /**
+    * @param {number} The data count that you want to get.
+    * @returns {object}
+    */
+
+    getTop(count) {
+        if (count < 0) {
+        return this.ranks.slice(0, 10)
+        } else if (count > this.rankCount) {
+        return this.ranks.slice(0, this.rankCount)
+        }
+    return this.ranks.slice(0, count)
+    }
+
+    /**
+     * @param {string} Player tag
+     * @returns {boolean}
+     */
+
+    isRanked(tag) {
+        return this.ranks.map(r => r.tag).includes(tag)
+    }
+}
+
+module.exports = Ranking
