fc9ae1c7f294b20972321f854abb014b33c17a11
Brawlup <86495381+brawlup@users.noreply.github.com>
2021-07-03 11:31:32 +0300

    Update player.js

--- src/player.js
+++ src/player.js
@@ -1,2 +1,43 @@
 class Player {
   constructor(data) {
+  this.tag = data.tag
+  this.name = data.name
+  this.icon = data.icon.id
+  this.nameColor = data.nameColor
+  this.hexColor = !this.nameColor ? '#ffffff' : `#${this.nameColor.slice(4)}`;
+  this.trophies = data.trophies
+  this.expLevel = data.expLevel
+  this.expPoints = data.expPoints
+  this.highestTrophies = data.highestTrophies
+  this.powerPlayPoints = data.powerPlayPoints
+  this.highestPowerPlayPoints = data.highestPowerPlayPoints
+  this.soloVictories = data.soloVictories
+  this.duoVictories = data.duoVictories
+  this.trioVictories = data['3vs3Victories']
+  this.totalVictories = parseInt(this.soloVictories + this.duoVictories + this.trioVictories)
+  this.bestRoboRumbleTime = data.bestRoboRumbleTime
+  this.bestTimeAsBigBrawler = data.bestTimeAsBigBrawler
+  this.brawlers = data.brawlers
+  this.brawlerCount = data.brawler.count
+  this.club = data.club.tag ? data.club : null
+  this.gadgetCount = data.brawlers.map(value => value.gadgets).flat().length.toString()
+  this.starPowerCount = data.brawlers.map(value => value.starPowers).flat().length.toString()
+}
+  
+  /**
+  * @param {string} The brawler's name or id.
+  * @description Finds the brawler from this player data.
+  * @returns null (if the brawler doesn't found or this player doesn't have this brawler) | Object (if the player have this brawler)
+  */
+  
+  findBrawler(brawler) {
+  if (!brawler) throw new TypeError(`You didn't specified a brawler, which is required for this method!`)
+ 
+  if (!isNaN(brawler)) {
+  let b = this.brawlers.filter(x => x.id == brawler)
+  return b ? b : false
+  } else {
+  let b = this.brawlers.filter(x => x.name == brawler.toUpperCase())
+  return b ? b : false
+  } 
+  }
