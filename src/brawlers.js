36a7bb951613e1bb1102033cb6f551237a82702f
Brawlup <86495381+brawlup@users.noreply.github.com>
2021-07-03 15:23:01 +0300

    Create brawlers.js

---
+++ src/brawlers.js
@@ -0,0 +1,29 @@
+const moduleError = require('./moduleError')
+
+class Brawlers {
+  constructor (data) {
+    this.all = data['items']
+    this.count = this.data.length
+  }
+  
+  /**
+  * @param {string} The brawler's name or id.
+  * @description Finds the brawler from the game.
+  * @returns null (if the brawler doesn't found) | Object (if the brawler does found)
+  */
+  
+  findBrawler(brawler) {
+  if (!brawler) throw new moduleError(`You didn't specified a brawler, which is required for this method!`)
+ 
+  if (!isNaN(brawler)) {
+  let b = this.all.filter(x => x.id == brawler)
+  return b ? b : false
+  } else {
+  let b = this.all.filter(x => x.name == brawler.toUpperCase())
+  return b ? b : false
+  } 
+  }
+  
+}
+
+module.exports = Brawlers
