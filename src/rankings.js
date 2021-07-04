28d99fbbc60b9fd10dcd204be07be80ed30e0d38
Brawlup <86495381+brawlup@users.noreply.github.com>
2021-07-04 20:39:33 +0300

    Update rankings.js

--- src/rankings.js
+++ src/rankings.js
@@ -7,20 +7,6 @@ class Rankings {
     }
 
     /**
-    * @param {number} The data count that you want to get.
-    * @returns {object}
-    */
-
-    getTop(count) {
-        if (count < 0) {
-        return this.ranks.slice(0, 10)
-        } else if (count > this.rankCount) {
-        return this.ranks.slice(0, this.rankCount)
-        }
-    return this.ranks.slice(0, count)
-    }
-
-    /**
      * @param {string} Player tag
      * @returns {boolean}
      */
