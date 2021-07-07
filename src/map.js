711ce601d4256073cc6076631c1729a366362163
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-07-08 01:21:38 +0300

    Update map.js

--- src/map.js
+++ src/map.js
@@ -12,7 +12,7 @@ class Map {
     this.madeBy = data.credit
     this.lastActive = data.lastActive
     this.dataUpdated = data.dataUpdated
-    this.enverioment = { name: data.environment.name, id: data.environment.id, image: data.environment.imageUrl }
+    this.environment = { name: data.environment.name, id: data.environment.id, image: data.environment.imageUrl }
     this.gameMode = { name: data.gameMode.name, id: data.gameMode.id, hash: data.gameMode.hash, link: data.gameMode.link, color: data.gameMode.color, image: data.gameMode.imageUrl }
     }
 
