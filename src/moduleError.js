51bce78e5df373cb3db2c30e8ff23f7c25324264
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-07-17 14:38:22 +0300

    Update moduleError.js

--- src/moduleError.js
+++ src/moduleError.js
@@ -2,7 +2,7 @@ class moduleError extends Error {
   constructor (err) {
     super()
 
-    this.message = `❌ brawlup.js error!\n\n📋 ${err}\n`
+    this.message = `❌ brawlup.js error!\n\n📋 ${err}\n🔗 Read the docs for more info: https://brawlup.js.org/js/#welcome`
   }
 }
 
