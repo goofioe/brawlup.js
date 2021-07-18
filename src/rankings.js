ecaf5494f0c66ab98cecaca64c79a25f5fd58ee2
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-07-18 12:49:51 +0300

    Update rankings.js

--- src/rankings.js
+++ src/rankings.js
@@ -12,6 +12,8 @@ class Rankings {
      */
 
     isRanked(tag) {
+        if (!tag) throw new moduleError(`You didn't specified an in-game player tag, which is required for this method!`)
+        if (typeof tag !== "string") throw new moduleError(`You didn't specified a valid type of player tag!`)
         return this.ranks.map(r => r.tag).includes(tag)
     }
 }
