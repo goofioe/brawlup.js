4f15338751b5aa261beae4f43ac5e9bb91a7d5f6
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-07-19 17:52:43 +0300

    2.0.0 !? - `index.js` update

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-exports.Brawlers = require('./src/brawlers.js')
-exports.Client = require('./src/client.js')
-exports.Player = require('./src/player.js')
-exports.AllMaps = require('./src/allMaps.js')
-exports.PowerLeagueMaps = require('./src/powerLeagueMaps.js')
-exports.Map = require('./src/map.js')
-exports.Events = require('./src/events.js')
-exports.BattleLog = require('./src/battlelog.js')
-exports.Club = require('./src/club.js')
-exports.Rankings = require('./src/rankings.js')
-exports.BrawlerRecords = require('./src/brawlerRecords.js')
+const pkgjson = require('../package.json')
+
+/* Define every single class under this! */
+
+const Client = require('./src/client.js')
+const Player = require('./src/player.js')
+const Club = require('./src/club.js')
+const BattleLog = require('./src/battlelog.js')
+const Brawlers = require('./src/brawlers.js')
+const Rankings = require('./src/rankings.js')
+const AllMaps = require('./src/allMaps.js')
+const PowerLeagueMaps = require('./src/powerLeagueMaps.js')
+const Map = require('./src/map.js')
+const Events = require('./src/events.js')
+
+/* 
+Define every single class above this! 
+Add every single class to the module.exports under this!
+*/
+
+module.exports = {
+  version: pkgjson.version,
+  
+  Client,
+  Player,
+  Club,
+  BattleLog,
+  Brawlers,
+  Rankings,
+  AllMaps,
+  PowerLeagueMaps,
+  Map,
+  Events
+  
+}
+
+/* Add every single class to the module.exports above this! */
