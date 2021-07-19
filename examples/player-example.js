86c4964d18f6f51754bfe713fea201417c403f30
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-07-19 13:18:00 +0300

    Update player-example.js

--- examples/player-example.js
+++ examples/player-example.js
@@ -1,1 +1,18 @@
+const bs = require('brawlup.js');
+const bsClient = new bs.Client('BrawlStarsAccessToken')
 
+;(async() => {
+ 
+ //Get the player from the client.
+ const player = await bsClient.getPlayer('#2YCC2P8U8')
+ 
+ //Expected: Alpu TV (#2YCC2P8U8)
+ console.log(`${player.name} (${player.tag})`)
+})()
+
+/* 
+
+                More Questions?
+See the docs here: https://brawlup.js.org/js/classes/player
+
+*/
