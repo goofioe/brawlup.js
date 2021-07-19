a567b02dcf40870f68d06ae07b63dd4801829655
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-07-19 17:35:45 +0300

    Update apiError.js

--- src/apiError.js
+++ src/apiError.js
@@ -9,7 +9,7 @@ class apiError extends Error {
     this.headers = res.headers
 
     this.reason = JSON.parse(body).message
-    .replace("Invalid authorization", "The access token is invalid! Get a new one: https://developer.brawlstars.com/#/account")
+    .replace("Invalid authorization", "The access token is invalid!")
 
     this.message = `${this.reason}\n🔗 ${this.url}\n`
   }
