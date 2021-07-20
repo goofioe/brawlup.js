ca339c8eb1a9cdf570ab740233341476d902cfce
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-07-20 23:58:13 +0300

    `2.0.0` - Fixes for the class

--- src/checkTeam.js
+++ src/checkTeam.js
@@ -13,40 +13,42 @@ constructor(brawler1, brawler2, brawler3) {
     if (typeof brawler1 !== "string") throw new moduleError(`You didn't specified a valid type of brawler! (first)`)
     if (typeof brawler2 !== "string") throw new moduleError(`You didn't specified a valid type of brawler! (second)`)
     if (typeof brawler3 !== "string") throw new moduleError(`You didn't specified a valid type of brawler! (third)`)
- 
+    
+    ;(async() => {
     const data = ourArray(await this.req.getPowerLeagueMaps()).find( ({ hash }) => hash === `${brawler1}+${brawler2}+${brawler3}` )
     
     if (!data) {
-    this.rating: { result: "Unknown", id: 0 }
-    this.winRate: null 
+    this.rating = { result: "Unknown", id: 0 }
+    this.winRate = null 
     }
  
     let winrate = data.data.winRate
     if (winrate < 47) { 
-    this.rating: { result: "Bad", id: 1 }
-    this.winRate: winrate
+    this.rating = { result: "Bad", id: 1 }
+    this.winRate = winrate
     }
     
     if (winrate > 47 && winrate < 51) { 
-    this.rating: { result: "Average", id: 2 }
-    this.winRate: winrate 
+    this.rating = { result: "Average", id: 2 }
+    this.winRate = winrate 
     }
   
     if (winrate > 52 && winrate < 65) {
-    this.rating: { result: "Good", id: 3 }
-    this.winRate: winrate 
+    this.rating = { result: "Good", id: 3 }
+    this.winRate = winrate 
     }
     
     if (winrate > 65 && winrate < 75) {
-    this.rating: { result: "Very Good", id: 3 }
-    this.winRate: winrate 
+    this.rating = { result: "Very Good", id: 3 }
+    this.winRate = winrate 
     }
     
     if (winrate > 75) { 
-    this.rating: { result: "Godly", id: 4 }
-    this.winRate: winrate 
+    this.rating = { result: "Godly", id: 4 }
+    this.winRate = winrate 
     }
-  
+  })()   
+      
   }
 }
 
