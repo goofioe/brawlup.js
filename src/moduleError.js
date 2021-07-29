76c1f22b9633d62fe9e1f15c83e2c6113ab68c5e
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-07-29 16:20:26 +0300

    2.0.0 - More serious errors 👨‍🏫

--- src/moduleError.js
+++ src/moduleError.js
@@ -2,7 +2,7 @@ class moduleError extends Error {
   constructor (err) {
     super()
     
-    this.name = '❌ brawlup.js Error'
+    this.name = 'brawlup.js Error'
 
     this.message = `${err}\n🔗 Read the docs for more info: https://brawlup.js.org/js/#welcome`
   }
