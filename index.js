3a6b1e0a1ef490908894f01f299a2498acc23793
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-07-29 17:33:55 +0300

    2.0.0 - No more MANAGERS...

--- index.js
+++ index.js
@@ -14,15 +14,6 @@ const PowerLeagueMaps = require('./src/powerLeagueMaps.js')
 const Map = require('./src/map.js')
 const Events = require('./src/events.js')
 
-const PlayerMngr = require('./src/managers/player.js')
-const ClubMngr = require('./src/managers/club.js')
-const BattleLogMngr = require('./src/managers/battlelog.js')
-const BrawlersMngr = require('./src/managers/brawlers.js')
-const RankingsMngr = require('./src/managers/rankings.js')
-const MapsMngr = require('./src/managers/maps.js')
-const PowerLeagueMapsMngr = require('./src/managers/powerLeagueMaps.js')
-const EventsMngr = require('./src/managers/events.js')
-
 /* 
 Define every single class above this! 
 Add every single class to the module.exports under this!
@@ -30,8 +21,6 @@ Add every single class to the module.exports under this!
 
 module.exports = {
   version: pkgjson.version,
-
-  /* Classes */
 
   Client,
   Player,
@@ -43,19 +32,7 @@ module.exports = {
   PowerLeagueMaps,
   Map,
   Events,
-  CheckTeam,
-
-  /* Managers */
-
-  PlayerMngr,
-  ClubMngr,
-  BattleLogMngr,
-  BrawlersMngr,
-  RankingsMngr,
-  MapsMngr,
-  PowerLeagueMapsMngr,
-  EventsMngr
-
+  CheckTeam
 }
 
 /* Add every single class to the module.exports above this! */
