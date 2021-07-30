8d9881b35d2780319b5bd22b0f0584ed7f5ad006
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-07-30 15:35:59 +0300

    2.0.0 - `apiError` in the docs!

--- src/apiError.js
+++ src/apiError.js
@@ -2,12 +2,31 @@ class apiError extends Error {
   constructor (res, body) {
     super()
 
-    this.name = 'BrawlStarsAPI Error'
+    this.name = 'BrawlStarsAPIError'
     this.res = res
+    
+   /**
+    * @description This error's url. (where it happened)
+    * @type {String}
+    */
     this.url = res.url
+    
+   /**
+    * @description This error's code.
+    * @type {Number}
+    */
     this.code = res.status
+    
+   /**
+    * @description This error's headers. (Bearer access token, application/json)
+    * @type {Object}
+    */
     this.headers = res.headers
 
+   /**
+    * @description This error's reason. (what/why happened)
+    * @type {String}
+    */
     this.reason = JSON.parse(body).message
 
     this.message = `${this.reason}\n🔗 ${this.url}\n`
