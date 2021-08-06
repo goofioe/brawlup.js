1db749fec493684ae9d3c4137d2f9597c3587dac
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-08-06 17:50:03 +0300

    2.0.0 - `ClubArray`

---
+++ src/clubArray.js
@@ -0,0 +1,12 @@
+const Client = require('./client')
+const moduleError = require('./moduleError')
+
+const Requesting = require('./requesting')
+
+class ClubArray extends Array {
+  constructor(data) {
+  
+  this.count = this.length
+}
+  
+module.exports = ClubArray
