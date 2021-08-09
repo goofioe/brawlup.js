e9cbf51db4e4eb0756a864d133bfa140ea5fb5ef
Brawlup <86495381+brawlup@users.noreply.github.com>
2021-08-09 22:53:36 +0300

    2.0.0 - Docs stuff

--- src/moduleError.js
+++ src/moduleError.js
@@ -1,12 +1,48 @@
+/**
+ * An error for this module.
+ * @extends {Error}
+ * @private
+ */
+
 class moduleError extends Error {
+  
+/**
+* An error for this module.
+* @param {string} err - The error reason/message.
+* @param {string} [type] Error type/error name.
+* @param {string} [brawler3] Third brawler of this team.
+* @private
+*/ 
+  
   constructor (err, type) {
     super()
     
     if (!type) {
-    this.name = ' Error' 
+    
+    /**
+    * This error's name.
+    * @type {string}
+    */
+    this.name = ' Error'
+      
+    /**
+    * This error's message. (which is logged)
+    * @type {string}
+    */
     this.message = `${err}\n🔗 https://brawlup.js.org/js/#welcome`
+    
     } else {
+      
+    /**
+    * This error's name.
+    * @type {string}
+    */
      this.name = type
+      
+    /**
+    * This error's message. (which is logged)
+    * @type {string}
+    */
      this.message = `${err}\n🔗 https://brawlup.js.org/js/#welcome`
     }
   }
