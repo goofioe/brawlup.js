3968880eed13fe3bfdedb791b8d96c1599c3ca67
Alpu TV <77716705+alputv@users.noreply.github.com>
2021-08-27 20:36:35 +0300

    2.2.0 - Client events!

---
+++ src/BaseClient.js
@@ -0,0 +1,33 @@
+const EventEmitter = require('events');
+
+/**
+ * The base class for the Brawl Stars client.
+ * @extends {EventEmitter}
+ */
+class BaseClient extends EventEmitter {
+  constructor(options = {}) {
+   super();
+    
+ }
+  
+  /**
+   * Increases max listeners by one, if they are not zero.
+   * @private
+   */
+  increaseMaxListeners() {
+    const maxListeners = this.getMaxListeners();
+    if (maxListeners !== 0) {
+      this.setMaxListeners(maxListeners + 1);
+    }
+  }
+
+  /**
+   * Decreases max listeners by one, if they are not zero.
+   * @private
+   */
+  decreaseMaxListeners() {
+    const maxListeners = this.getMaxListeners();
+    if (maxListeners !== 0) {
+      this.setMaxListeners(maxListeners - 1);
+    }
+  }
